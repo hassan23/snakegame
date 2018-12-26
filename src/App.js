f8dfa4ebd8fc7fa1cf125d5d2e205802482d45f6
hassan23 <sr.hassan23@gmail.com>
2018-12-26 23:46:28 +0530

    Commenting the App

--- src/App.js
+++ src/App.js
@@ -2,34 +2,21 @@ import React, { Component } from "react";
 import _ from "lodash";
 import "./App.css";
 
-class Cell extends Component {
-  render() {
-    return (
-      <div
-        key={this.props.cellKey}
-        id={this.props.id}
-        className={this.props.snakeClass}
-      />
-    );
-  }
-}
-
+/**
+ * @description create the grid on the screen on each interval with the position of snake
+ * @class Forest
+ * @extends {Component}
+ */
 class Forest extends Component {
   DrawGrid = () => {
     let self = this;
     return _.map(_.range(900), function(i) {
+      // gird is 30 X 30
       let id = i + 1;
       let snakeClass =
-        self.props.snake.indexOf(id) > -1 ? "cell snake" : "cell";
+        self.props.snake.indexOf(id) > -1 ? "cell snake" : "cell"; // cell snake class represents the presence of snake at that position
       if (id === self.props.food) snakeClass = "cell food";
-      return (
-        <Cell
-          snakeClass={snakeClass}
-          key={"cell" + id}
-          cellKey={"cell" + id}
-          id={"cell" + id}
-        />
-      );
+      return <div className={snakeClass} key={"cell" + id} id={"cell" + id} />;
     });
   };
 
@@ -38,18 +25,23 @@ class Forest extends Component {
   }
 }
 
-class App extends Component {
+/**
+ * @description Main Snake Game class
+ * @class App
+ * @extends {Component}
+ */
+class Snake extends Component {
   constructor(props) {
     super(props);
-    this.speed = 200;
+    this.speed = 200; // default speed
     this.score = 0;
-    this.start = true;
-    this.step = 1;
+    this.start = true; // only true for the first game
+    this.step = 1; // no of steps defines the direction (1 for right, -1 for left, 30 for down, -30 for up)
     this.mode = "Easy";
     this.gameOver = false;
     this.state = {
-      snake: [1, 2, 3, 4, 5],
-      food: parseInt(Math.random() * 900) + 1
+      snake: [1, 2, 3, 4, 5], // initial length of snake and in which cells of forest snake is in
+      food: parseInt(Math.random() * 900) + 1 // cell no where food for snake is present
     };
   }
 
@@ -57,18 +49,21 @@ class App extends Component {
     document.addEventListener("keydown", this.startGame);
   };
 
+  // this call responsible for snakes movement and food serving
   walk = () => {
     this.start = false;
     let snake = this.state.snake;
-    this.updateSpeedMode();
+
+    this.updateSpeedMode(); // increase the speed if score crossed a limit
+
     if (this.gameOver) {
-      clearInterval(this.SnakeInter);
+      clearInterval(this.SnakeInter); // stops the game
     } else if (this.isGameOver()) {
       console.log("Game Over");
       this.gameOver = true;
       this.setState({ snake: snake });
     } else {
-      snake.push(snake[snake.length - 1] + this.step);
+      snake.push(snake[snake.length - 1] + this.step); // move the snake in certain direction
       if (this.state.food !== snake[snake.length - 1]) {
         snake = snake.slice(1);
         this.setState({ snake: snake });
@@ -83,6 +78,7 @@ class App extends Component {
     }
   };
 
+  //updating the speed of snake
   updateSpeedMode = () => {
     if (this.score > 10 && this.score < 20) {
       this.speed = 100;
@@ -97,6 +93,8 @@ class App extends Component {
       this.SnakeInter = setInterval(this.walk, this.speed);
     }
   };
+
+  // cheking if snake hits the wall or make a loop
   isGameOver = () => {
     let snake = this.state.snake;
     if (snake.length !== _.uniq(snake).length) {
@@ -117,6 +115,8 @@ class App extends Component {
       }
     }
   };
+
+  // restarting the whole game
   reset = () => {
     this.speed = 200;
     this.score = 0;
@@ -128,6 +128,8 @@ class App extends Component {
       food: parseInt(Math.random() * 900) + 1
     });
   };
+
+  // initiating and controlling the game throw key presses
   startGame = e => {
     //console.log(e);
     if (e.key === "Enter") {
@@ -162,14 +164,14 @@ class App extends Component {
         <h4>{this.gameOver ? "Game Over: Press ENTER to play Again" : ""}</h4>
         <h3>
           {this.start
-            ? "Press Enter to play"
+            ? "Welcome to the Snake Game: Press Enter to play"
             : this.gameOver
             ? ""
-            : "Mode:" + this.mode}
+            : "Mode: " + this.mode}
         </h3>
       </div>
     );
   }
 }
 
-export default App;
+export default Snake;
